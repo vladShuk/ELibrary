1e4fafaa2a58e5b2ac4b1b4a7aa50831a62551d1
ShukVlad <shukvlad@gmail.com>
2017-02-01 08:16:30 +0300

    Update DAO.java

--- src/by/ELibrary/DAO.java
+++ src/by/ELibrary/DAO.java
@@ -85,6 +85,67 @@ public class DAO {
 		ResultSet rs = st.executeQuery("SELECT * FROM books ORDER BY id DESC LIMIT " + (page-1)*5 + ", " + page*5);
 		List<Book> list = new ArrayList<Book>();
 		while(rs.next()) {
+
+public class DAO {
+	private static final String path = "jdbc:mysql://localhost:3306/ELibrary";
+	private static final String pathLogin = "root";
+	private static final String pathPassword = "1091";
+
+	// Добавление новой книги
+	static int addBook(String name, String author, int year, String description) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
+		Statement st = cn.createStatement();
+		st.execute("INSERT INTO books (name, author, year, description) VALUES('" + name + "', '" + author + "', " + year + ", '" + description + "')");
+		ResultSet rs = st.executeQuery("select last_insert_id() AS id1 from books");
+		int count = 0;
+		if (rs.next())
+			count = rs.getInt("id1");
+		cn.close();
+		
+		return count;
+	}
+	
+	// Установка контента для новой книги
+	static void setContent(int id, String content) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
+		Statement st = cn.createStatement();
+		st.execute("UPDATE books SET content = '" + content + "' WHERE id = '" + id + "'");
+		cn.close();
+	}
+	
+	// Получение книги
+	static Book getBook(int id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
+		Statement st = cn.createStatement();
+	
+		ResultSet rs = st.executeQuery("SELECT * FROM books WHERE id = " + id);
+		Book book = null;
+		while(rs.next()) {
+			int rsId = rs.getInt("id");
+			String rsName = rs.getString("name");
+			String rsAuthor = rs.getString("author");
+			int rsYear = rs.getInt("year");
+			String rsDescription = rs.getString("description");
+			String rsContent = rs.getString("content");
+			book = new Book(rsId, rsName, rsAuthor, rsYear, rsDescription, rsContent);
+		}	
+		cn.close();
+		
+		return book;
+	}
+	
+	// Получение последних 5 книг
+	static List<Book> listBooks() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
+		Statement st = cn.createStatement();
+	
+		ResultSet rs = st.executeQuery("SELECT * FROM books ORDER BY id DESC LIMIT 5");
+		List<Book> list = new ArrayList<Book>();
+		while(rs.next()) {
 			int rsId = rs.getInt("id");
 			String rsName = rs.getString("name");
 			String rsAuthor = rs.getString("author");
@@ -98,6 +159,29 @@ public class DAO {
 		return list;
 	}
 	
+	// Получение 5 книг для определенной страницы
+	static List<Book> listBooks(int page) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
+		Statement st = cn.createStatement();
+	
+		ResultSet rs = st.executeQuery("SELECT * FROM books ORDER BY id DESC LIMIT " + (page-1)*5 + ", " + page*5);
+		List<Book> list = new ArrayList<Book>();
+		while(rs.next()) {
+			int rsId = rs.getInt("id");
+			String rsName = rs.getString("name");
+			String rsAuthor = rs.getString("author");
+			int rsYear = rs.getInt("year");
+			String rsDescription = rs.getString("description");
+			String rsContent = rs.getString("content");
+			list.add(new Book(rsId, rsName, rsAuthor, rsYear, rsDescription, rsContent));
+		}	
+		cn.close();
+		
+		return list;
+	}
+	
+	// Получение количества страниц (для пагинации)
 	static int countOfPages() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
@@ -111,6 +195,7 @@ public class DAO {
 		return count/5 + ((count%5==0)?0:1);
 	}
 	
+	// Поиск строки среди названий книг и авторов
 	static List<Book> searchBooks(String string) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
@@ -131,6 +216,7 @@ public class DAO {
 		return list;
 	}
 	
+	// Изменение книги
 	static void editBook(int id, String name, String author, int year, String description) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
@@ -139,6 +225,7 @@ public class DAO {
 		cn.close();
 	}
 	
+	// Удаление книги
 	static String deleteBook(int id) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection cn = DriverManager.getConnection(path, pathLogin, pathPassword);
