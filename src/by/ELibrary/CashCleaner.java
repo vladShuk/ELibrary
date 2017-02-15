2cb002b3d7e6c4c995d9f77faa2417eb468e7657
vladShuk <vladey_@list.ru>
2017-02-15 22:28:53 +0300

    Update CashCleaner.java

--- src/by/ELibrary/CashCleaner.java
+++ src/by/ELibrary/CashCleaner.java
@@ -2,6 +2,7 @@ package by.ELibrary;
 
 import java.util.concurrent.TimeUnit;
 
+// Класс для очистки кэша приложения от книг, к которым не обращались последние 60 секунд
 public class CashCleaner implements Runnable {
 	private int key;
 	
